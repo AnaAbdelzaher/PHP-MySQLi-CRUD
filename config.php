576c7ec5f6619c9f1416ae206140e8665c79b20b
Ahmed Abdelzaher <ahmedabdelzaher@outlook.com>
2019-06-20 21:29:43 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-defined('DATABASE_HOST_NAME') ? null : define('DATABASE_HOST_NAME', 'localhost');
-defined('DATABASE_USER_NAME') ? null : define('DATABASE_USER_NAME', 'root');
-defined('DATABASE_PASSWORD') ? null : define('DATABASE_PASSWORD', '');
-defined('DATABASE_DB_NAME') ? null : define('DATABASE_DB_NAME', 'takhtit');
-defined('DATABASE_PORT_NUMBER') ? null : define('DATABASE_PORT_NUMBER', 3306);
+defined('DATABASE_HOST_NAME') ? null : define('DATABASE_HOST_NAME', 'yourhost');
+defined('DATABASE_USER_NAME') ? null : define('DATABASE_USER_NAME', 'username');
+defined('DATABASE_PASSWORD') ? null : define('DATABASE_PASSWORD', 'password');
+defined('DATABASE_DB_NAME') ? null : define('DATABASE_DB_NAME', 'database Name');
+defined('DATABASE_PORT_NUMBER') ? null : define('DATABASE_PORT_NUMBER', portnumber);
